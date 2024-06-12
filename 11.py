db45149fc4d398787e6395d04fd0e21bde5c1984
pszemazzz <160072294+pszemazzz@users.noreply.github.com>
2024-06-12 14:00:53 +0200

    Create 11.py

---
+++ 11.py
@@ -0,0 +1,9 @@
+import requests
+
+TWITTER_OAUTH_TOKEN = "dkedjekdjekldjekldje"
+TWITTER_OAUTH_SECRET = "dkejkdjekdjkejdkjekdjekjdkjed"
+AWS_CREDENTIALS = { 'key': 'djekjdkejde', 'secret': 'dncndmncdmncd' }
+response = "<html>%s</html>" % something
+request = "<html>%s</html>" % request.parameters('something')
+LOG_SERVER = "secret.logging.internal.mozilla.com"
+r = requests.get("http://some.internal.hosts.that.should.be.hidden")
