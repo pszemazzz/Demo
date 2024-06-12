351859f3924d2ffa6b83099a8d5d619c82cd980e
pszemazzz <160072294+pszemazzz@users.noreply.github.com>
2024-06-12 12:25:24 +0200

    Create vuln.vue

---
+++ vuln.vue
@@ -0,0 +1,9 @@
+<template>
+  <div v-html="getData(maliciousData)"></div>
+</template>
+<script setup>
+const maliciousData = "<img src=x onerror=alert(1)//>";
+function getData(data) {
+  return data;
+}
+</script>
